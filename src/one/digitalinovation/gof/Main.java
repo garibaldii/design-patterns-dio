8f47f42fd938e7c662265bd0bc4442360f6709af
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-07-23 16:18:18 -0300

    refactor: commented singleton session and add strategy

--- src/one/digitalinovation/gof/Main.java
+++ src/one/digitalinovation/gof/Main.java
@@ -1,22 +1,71 @@
 package one.digitalinovation.gof;
 
+import one.digitalinovation.gof.singleton.EagerSingleton;
+import one.digitalinovation.gof.singleton.LazyHolderSingleton;
+import one.digitalinovation.gof.singleton.LazySingleton;
+import one.digitalinovation.gof.strategy.*;
+
+import java.util.concurrent.TimeoutException;
+
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        LazySingleton lazy = LazySingleton.getInstance();
-        System.out.println(lazy);
-        lazy = LazySingleton.getInstance();
-        System.out.println(lazy);
 
-        EagerSingleton eager = EagerSingleton.getInstance();
-        System.out.println(eager);
-        eager = EagerSingleton.getInstance();
-        System.out.println(eager);
+        //Singleton
 
-        LazyHolderSingleton lazyHolder = LazyHolderSingleton.getInstance();
-        System.out.println(lazyHolder);
-        lazyHolder = LazyHolderSingleton.getInstance();
-        System.out.println(lazyHolder);
+        /**
+         *         LazySingleton lazy = LazySingleton.getInstance();
+         *         System.out.println(lazy);
+         *         lazy = LazySingleton.getInstance();
+         *         System.out.println(lazy);
+         *
+         *         EagerSingleton eager = EagerSingleton.getInstance();
+         *         System.out.println(eager);
+         *         eager = EagerSingleton.getInstance();
+         *         System.out.println(eager);
+         *
+         *         LazyHolderSingleton lazyHolder = LazyHolderSingleton.getInstance();
+         *         System.out.println(lazyHolder);
+         *         lazyHolder = LazyHolderSingleton.getInstance();
+         *         System.out.println(lazyHolder);
+         *
+         */
+
+
+        //Strategy
+        //pode-se assimilar o conceito a um gerenciamento de veículos, ou estacionamento
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    System.out.println("Error");
+                }
+            }
+        };
+
+        Thread thread = new Thread(runnable);
+
+        Behavior normal = new NormalBehavior();
+        Behavior agressive = new AgressiveBehavior();
+        Behavior defensive = new DefensiveBehavior();
+
+        Robot robot = new Robot();
+
+        robot.setBehavior(agressive);
+        robot.move();
+
+
+
+        robot.setBehavior(defensive);
+        robot.move();
+
+
+        robot.setBehavior(normal);
+        robot.move();
     }
 }
